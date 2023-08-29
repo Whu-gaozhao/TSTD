2150ce6eadee756dfd4a5e80f4e2074bc86db9bf
Whu-gaozhao <92943212+Whu-gaozhao@users.noreply.github.com>
2023-08-29 19:10:19 +0800

    Delete scrips directory

--- scrips/read_pkl.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-
-
-data = '/home/zhao/DataSets/scannet/change/scannet_infos_test.pkl'
-with open(data, "rb") as fp_data:
-    data_pkl = pickle.load(fp_data)
-    print(data_pkl)
-    print(len(data_pkl))
